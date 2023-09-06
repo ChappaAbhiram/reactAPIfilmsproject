f29d4db803338fbfff20d04741806cf3471ff6b8
ChappaAbhiram <abhiramchappa@gmail.com>
2023-09-06 16:56:14 +0530

    Form to add movies is made

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import MoviesList from './components/MoviesList';
 import './App.css';
+import AddMovie from './components/AddMovie';
 
 function App() {
   const [movies, setMovies] = useState([]);
@@ -47,6 +48,11 @@ function App() {
     fetchMoviesHandler();
   },[fetchMoviesHandler]);
 
+  function addMovieHandler(movie) {
+    console.log(movie);
+  }
+
+
   function handleCancelRetry() {
     setRetrying(false); // Stop retrying
   }
@@ -69,6 +75,9 @@ function App() {
 
   return (
     <React.Fragment>
+       <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
